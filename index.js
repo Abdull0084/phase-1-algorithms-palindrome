884101d4d86de394ab85d6339f6df9575a369f39
Abdull0084 <Abdimalik0084@gmail.com>
2022-12-13 18:35:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
+function reverse(word) {
+  // 'abc' => 'cba'
+  // const wordArray = word.split("");
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  return word.split("").reverse().join("");
+}
+ 
 
 /* 
   Add your pseudocode here
+   that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
